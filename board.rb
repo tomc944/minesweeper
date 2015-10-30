3dc5bdde21a4e6aa9d9a49080cb4fec26f547808
App Academy Student <student@appacademy.io>
2015-10-30 14:36:27 -0700

    finished board class

--- board.rb
+++ board.rb
@@ -1,3 +1,5 @@
+require_relative 'tile.rb'
+
 class Board
   attr_accessor :grid
 
@@ -7,6 +9,13 @@ class Board
 
   def populate(bombs)
     grid = Array.new(9) { Array.new(9) { Tile.new(0)} }
+    disperse_bombs(grid, bombs)
+    assign_bomb_counts(grid)
+  end
+
+  private
+
+  def disperse_bombs(grid, bombs)
     bombs_left = bombs
 
     while bombs_left > 0
@@ -17,23 +26,43 @@ class Board
         bombs_left -= 1
       end
     end
+    grid
+  end
 
-    # get neighbor bomb count
-    # assign that to tile value
-    # ignore if at bomb
-    # yay commits lunch time
-
+  def assign_bomb_counts(grid)
+    grid.each_with_index do |row, row_i|
+      row.each_with_index do |tile, col|
+        unless tile.value == :bomb
+          tile.value = neighbor_bomb_count(row_i, col)
+        end
+      end
+    end
+    grid
   end
 
   def neighbors(pos)
-    current_tile = @grid[pos[0]][pos[1]]
+    neighbors = []
+    x_pos = pos[0]
+    y_pos = pos[1]
+
+    current_tile = @grid[x_pos][y_pos]
+
     #loop through some shit
-    #check if valid
-    #return array
+    (-1..1).each do |x|
+      (-1..1).each do |y|
+        new_x = x_pos + x
+        new_y = y_pos + y
+        unless (x == 0 && y == 0) || new_x.between?(0,8) || new_y.between?(0,8)
+          neighbors << @grid[new_x][new_y]
+        end
+      end
+    end
+
+    neighbors
   end
 
   def neighbor_bomb_count(pos)
-    neighbors(pos)#count bomb values
+    neighbors(pos).count{ |tile| tile.value == :bomb }
   end
 
 
